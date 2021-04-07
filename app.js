dca045d432afeec18e48605aa2ad0b14fe640590
Salvador <salvador30@live.com.pt>
2021-04-07 18:23:24 +0100

    Cenas

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1>")
+});
+
+app.listen(8080, () => {
+    console.log("Server started on Port 8080");
+});
